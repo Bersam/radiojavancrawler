a9e065cba2e39f633ff04133c7c7716703780c02
Bersam Karbasion <bersam.k@gmail.com>
2015-03-15 16:23:15 +0330

    using cheerio and crawler it's much more simpler

--- main.js
+++ main.js
@@ -1,57 +1,40 @@
-// Example use of simplecrawler, courtesy of @breck7! Thanks mate. :)
-var fs = require('node-fs'),
-    url = require('url'),
-    wrench = require('wrench'),
-    Crawler = require("simplecrawler").Crawler;
-/**
- * @Param String. Domain to download.
- * @Param Function. Callback when crawl is complete.
- */
-var downloadSite = function (domain, callback) {
-    // Where to save downloaded data
-    var outputDirectory = __dirname + '/' + domain;
-    var myCrawler = new Crawler(domain);
-    myCrawler.interval = 250;
-    myCrawler.maxConcurrency = 5;
-    myCrawler.on("fetchcomplete",function(queueItem, responseBuffer, response) {
-        // Parse url
-        var parsed = url.parse(queueItem.url);
-        // Rename / to index.html
-        if (parsed.pathname === '/'){
-            parsed.pathname = '/index.html';
-        }
-        // Get directory name in order to create any nested dirs
-        var dirname = outputDirectory + parsed.pathname.replace(/\/[^\/]+$/, '');
-        // Path to save file
-        var filepath = outputDirectory + parsed.pathname;
-        // Check if DIR exists
-        fs.exists(dirname, function (exists) {
-            // If DIR exists, write file
-            if (exists)
-                fs.writeFile(filepath, responseBuffer, function () {});
-            // Else, recursively create dir using node-fs, then write file
-            else
-                fs.mkdir(dirname, 0755, true, function (err) {
-                    fs.writeFile(filepath, responseBuffer, function () {});
-                });
-        });
+var Crawler = require("crawler");
+var url = require('url');
 
-        console.log("I just received %s (%d bytes)",queueItem.url,responseBuffer.length);
-        console.log("It was a resource of type %s",response.headers['content-type']);
+var c = new Crawler({
+  maxConnections : 10,
+  // This will be called for each crawled page
+  jQuery: 'cheerio',
+  callback : function (error, result, $) {
+    // $ is Cheerio by default
+    //a lean implementation of core jQuery designed specifically for the server
+    $('a.mp3_download_link').each(function(index, a) {
+      var downloadLink = $(a).attr('href');
+      console.log(downloadLink);
     });
-    // Fire callback
-    myCrawler.on('complete', function () {
-        callback();
-    });
-    // Start Crawl
-    myCrawler.start();
-};
-if (process.argv.length < 3) {
-    console.log('Usage: node downloadSiteExample.js mysite.com');
-    process.exit(1);
-}
-downloadSite(process.argv[2], function () {
-    console.log('Done!');
+    console.log($('title').text());
+  }
 });
 
+if (process.argv.length < 3) {
+  console.log('Usage: node main.js ddab01a43208');
+  process.exit(1);
+}
 
+// Queue just one URL, with default callback
+c.queue([{
+  uri: 'http://www.radiojavan.com/playlists/playlist/mp3/'+process.argv[2],
+  jQuery: 'cheerio',
+  // The global callback won't be called
+  callback: function (error, result, $) {
+    playlist = $('#playlist');
+    songs = playlist.find('.simple_table tr td:first-child a');
+    songs.each(function(index, a){
+      if(index % 3 === 0){
+        var song = "http://www.radiojavan.com"+$(a).attr('href');
+        c.queue(song);
+      }
+    });
+    console.log($('title').text());
+  }
+}]);
